90c211183679138d790272608f1aad81a3e3975c
gemreyranola <gemreyranola@gmail.com>
2023-12-04 15:01:12 +0800

    Update activity5_restaurant.py

--- MIdterm/activity5_restaurant.py
+++ MIdterm/activity5_restaurant.py
@@ -1,32 +1,32 @@
 class MenuItem:
     def __init__(self, item_name: str, price: float, description: str) -> None:
-        self.item_name = item_name
-        self.price = price
-        self.description = description
+        self.__item_name = item_name
+        self.__price = price
+        self.__description = description
 
     def display_details(self):
-        print(f'Item Name:\t{self.item_name.title()}\nPrice:\t\t{self.price}\nDescription:\t{self.description.title()}')
+        print(f'Item Name:\t{self.get_item_name().title()}\nPrice:\t\t{self.get_price()}\nDescription:\t{self.get_description().title()}')
 
     def get_item_name(self):
-        return self.item_name
+        return self.__item_name
 
     def get_price(self):
-        return self.price
+        return self.__price
 
     def get_description(self):
-        return self.description
+        return self.__description
 
     def update_item(self, item_name: str=None, price: float=None, description: str=None):
         if item_name is not None:
-            self.item_name = item_name
+            self.__item_name = item_name
             msg += f'\nItem name changed to {item_name}'
 
         if price is not None:
-            self.price = price
+            self.__price = price
             msg += f'\nPrice Updated to {price}'
 
         if description is not None:
-            self.description = description
+            self.__description = description
             msg += f'\nDescription changed to {description}'
         msg = ''
         print(msg)
@@ -40,7 +40,7 @@ class Order:
             self.orders.append(order_item)
             print(f'The {order_item.get_item_name().title()} in the order list.')
         else:
-            print(f'The {order_item.get_item_name().title()} is not Ready.')
+            print(f'You need to cook {order_item.get_item_name().title()} to add it in the order.')
 
     def calculate_total(self):
         total = 0
@@ -55,7 +55,7 @@ class FoodItem(MenuItem):
 
     def cook_food(self):
         self.cook = True
-        print(f'The {self.item_name.title()} has been cooked!')
+        print(f'The {self.get_item_name().title()} has been cooked!')
 
     def get_status(self):
         return self.cook
@@ -67,7 +67,7 @@ class DrinkItem(MenuItem):
 
     def pour(self):
         self.poured = True
-        print(f'The {self.item_name.title()} has been Poured!')
+        print(f'The {self.get_item_name().title()} has been Poured!')
 
     def get_status(self):
         return self.poured
@@ -76,26 +76,15 @@ class PremiumFoodItem(FoodItem):
     def __init__(self) -> None:
         super().__init__()
 
+order1 = Order()
 pizza = FoodItem('pizza', 20.0, 'A Rounded Food and yummy')
-burger = FoodItem('burger', 37.0, 'A bigmak')
-nachos = FoodItem('nachos', 38.0, 'A chips')
+order1.add_item(pizza)
 
-burger.cook_food()
 pizza.cook_food()
-nachos.cook_food()
-
+pizza.display_details()
 redhorse = DrinkItem('redhorse', 20.0, 'A beer')
 redhorse.pour()
 
-order1 = Order()
 order1.add_item(pizza)
-order1.add_item(burger)
 order1.add_item(redhorse)
-order1.add_item(nachos)
 order1.calculate_total()
-
-ha = input('')
-sa = input('>')
-ha = sa
-
-print(f'The number is {ha}')
